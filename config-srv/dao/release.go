45052ab974aa1606a1884fadb94b135cee9fdde8
Allenxuxu <120582243@qq.com>
2019-12-07 19:28:20 +0800

    update config-srv release

--- config-srv/dao/release.go
+++ config-srv/dao/release.go
@@ -1,16 +1,10 @@
 package dao
 
 import (
-	"errors"
-
 	"github.com/Allenxuxu/XConf/config-srv/model"
 )
 
 func (d *Dao) ReleaseConfig(appName, clusterName, namespaceName, value, comment string, broadcast func() error) error {
-	if !d.NamespaceExist(appName, clusterName, namespaceName) {
-		return errors.New("namespace not found")
-	}
-
 	tx := d.client.Begin()
 
 	if err := tx.Table("release").Create(&model.Release{
