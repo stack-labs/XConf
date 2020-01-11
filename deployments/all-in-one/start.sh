d4bb6443173c4afeecebd94080c8c2d84a2dafde
Allenxuxu <120582243@qq.com>
2020-01-11 13:00:25 +0800

    update start.sh

--- deployments/all-in-one/start.sh
+++ deployments/all-in-one/start.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
+set -e
 
 /root/config-srv &
 /root/agent-api &
 /root/admin-api &
-/root/micro --cors-allowed-headers="Content-Type,X-Token" --cors-allowed-origins="*" --cors-allowed-methods="OPTIONS,DELETE,GET,POST" api --handler=http
+/root/micro --cors-allowed-headers="Content-Type,X-Token" --cors-allowed-origins="*" --cors-allowed-methods="OPTIONS,DELETE,GET,POST" api --handler=http &
+
+
+check(){
+  local process=`ps -ef| grep $1 | grep -v grep`;
+  if [ "$process" == "" ]; then
+        echo "$1 exit";
+        exit -1
+  fi
+}
+
+while true
+do
+  sleep 2s
+  check "config-srv"
+  check "agent-api"
+  check "admin-api"
+  check "micro"
+done
