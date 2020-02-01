1307d050da8fedc249dcc2743c1a5225ec703ae1
Allenxuxu <120582243@qq.com>
2020-02-01 15:37:19 +0800

    update dashboard navbar

--- dashboard/src/layout/components/Navbar.vue
+++ dashboard/src/layout/components/Navbar.vue
@@ -16,15 +16,15 @@
               Home
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+          <a target="_blank" href="https://github.com/micro-in-cn/XConf">
             <el-dropdown-item>Github</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
-          <el-dropdown-item divided>
-            <span style="display:block;" @click="logout">Log Out</span>
-          </el-dropdown-item>
+          <!--          <a target="_blank" href="/">-->
+          <!--            <el-dropdown-item>Docs</el-dropdown-item>-->
+          <!--          </a>-->
+          <!--          <el-dropdown-item divided>-->
+          <!--            <span style="display:block;" @click="logout">Log Out</span>-->
+          <!--          </el-dropdown-item>-->
         </el-dropdown-menu>
       </el-dropdown>
     </div>
