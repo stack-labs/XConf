2bdf1cd728ad9b8dee4442ec7805202c914ccbd1
Allenxuxu <120582243@qq.com>
2020-03-17 12:26:05 +0800

    add watch raw

--- agent-api/handler/handler.go
+++ agent-api/handler/handler.go
@@ -90,3 +90,41 @@ func WatchUpdate(c *gin.Context) {
 	// TODO 幂等？
 	c.JSON(http.StatusOK, value)
 }
+
+func WatchUpdateRaw(c *gin.Context) {
+	var req = struct {
+		AppName       string `form:"appName"        binding:"required"`
+		ClusterName   string `form:"clusterName"    binding:"required"`
+		NamespaceName string `form:"namespaceName"  binding:"required"`
+		UpdatedAt     int64  `form:"updatedAt"`
+	}{}
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
+		return
+	}
+
+	w := config.Watch(req.AppName, req.ClusterName, req.NamespaceName)
+	time.AfterFunc(time.Second*60, func() {
+		_ = w.Stop()
+	})
+	if req.UpdatedAt > 0 {
+		value, err := config.ReadConfig(req.AppName, req.ClusterName, req.NamespaceName)
+		if err != nil {
+			log.Errorf(err.Error())
+		} else {
+			if value.UpdatedAt > req.UpdatedAt {
+				c.JSON(http.StatusOK, value)
+				return
+			}
+		}
+	}
+
+	value, err := w.Next()
+	if err != nil {
+		c.JSON(http.StatusNotModified, gin.H{})
+		return
+	}
+
+	// TODO 幂等？
+	c.String(http.StatusOK, value.Value)
+}
