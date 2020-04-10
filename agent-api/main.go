e3c5ba16eebd70d7ca8db5f03befa977ded4d471
Allenxuxu <120582243@qq.com>
2020-04-10 19:07:13 +0800

    agent-api 支持参数/环境遍历设置 cache size

--- agent-api/main.go
+++ agent-api/main.go
@@ -5,12 +5,27 @@ import (
 	"github.com/micro-in-cn/XConf/agent-api/config"
 	"github.com/micro-in-cn/XConf/agent-api/handler"
 	pconfig "github.com/micro-in-cn/XConf/proto/config"
+	"github.com/micro/cli"
 	"github.com/micro/go-micro/web"
 )
 
 func main() {
+	var cacheSize int
+
 	service := web.NewService(
 		web.Name("go.micro.api.agent"),
+		web.Flags(
+			cli.IntFlag{
+				Name:        "cache_size",
+				Usage:       "cache size (k)",
+				EnvVar:      "XCONF_CACHE_SIZE",
+				Value:       1024 * 1024,
+				Destination: nil,
+			},
+		),
+		web.Action(func(context *cli.Context) {
+			cacheSize = context.Int("cache_size")
+		}),
 	)
 
 	if err := service.Init(); err != nil {
@@ -19,7 +34,7 @@ func main() {
 
 	client := pconfig.NewConfigService("go.micro.srv.config", service.Options().Service.Client())
 
-	config.Init(client, 1024*1024)
+	config.Init(client, cacheSize*1024)
 	router := Router()
 	service.Handle("/", router)
 
