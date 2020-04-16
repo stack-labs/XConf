229cdb082cd84df84564c3a1b30fa668ac5d6488
56 <sherryqueenlove@gmail.com>
2020-04-16 20:01:35 +0800

    chore: Upgrade version

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: ['react-app', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  extends: ['react-app', 'plugin:prettier/recommended', 'plugin:react/recommended'],
   rules: {
     'no-var': 'error',
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
@@ -13,5 +14,8 @@ module.exports = {
         memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
       },
     ],
+    // * prettier-react
+    'react/prop-types': [0],
+    'react/display-name': [0],
   },
 };
